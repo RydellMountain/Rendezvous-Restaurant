94bc7cffb87a0a1080715d1a46c25a4bcaa570ad
Damian Govender <damiankenryu@gmail.com>
2021-05-17 22:20:54 +0200

    Added SubCategoryController in Admin Area

---
+++ AppDevProjectGroup27/Areas/Admin/Controllers/SubCategoryController.cs
@@ -0,0 +1,21 @@
+﻿using AppDevProjectGroup27.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppDevProjectGroup27.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SubCategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public SubCategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+       
+
+    }
+}
