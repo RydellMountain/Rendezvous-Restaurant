c19d273e9cdf275c20ba5238afbf6ad25d7baa45
Rydell moutain Thomas <21900109@dut4life.ac.za>
2021-05-18 10:51:02 +0200

    Added SubcatgoryAndCategoryViewModel

---
+++ AppDevProjectGroup27/Models/ViewModels/SubCatgoryAndCategoryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppDevProjectGroup27.Models.ViewModels
+{
+    public class SubCatgoryAndCategoryViewModel
+    {
+        public IEnumerable<Category> CategoryList { get; set; }
+
+        public SubCategory SubCategory { get; set; }
+
+        public List<string> SubCategoryList { get; set; }
+
+        public string StatusMessage { get; set; }
+    }
+}
