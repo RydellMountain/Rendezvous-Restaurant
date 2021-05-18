cc913a76da6d6473fda7bc2cd410ca7a3169a132
Damian Govender <damiankenryu@gmail.com>
2021-05-18 13:22:39 +0200

    Added IEnumerableExtension Class to Extensions folder

---
+++ AppDevProjectGroup27/Extensions/IEnumerableExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppDevProjectGroup27.Extensions
+{
+    public class IEnumerableExtension
+    {
+
+    }
+}
