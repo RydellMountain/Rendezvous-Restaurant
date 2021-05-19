40713bfbb4b18c298be251eff4535e9b8bf222f6
spheleletholy262000@gmail.com <spheleletholy262000@gmail.com@users.noreply.github.com>
2021-05-19 15:14:10 +0200

    Added a MenuItemController

---
+++ AppDevProjectGroup27/Areas/Admin/Controllers/MenuItemsController.cs
@@ -0,0 +1,28 @@
+﻿using AppDevProjectGroup27.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppDevProjectGroup27.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MenuItemsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        private readonly IHostingEnvironment hostingEnvironment;
+        public MenuItemsController(ApplicationDbContext db, IHostingEnvironment hostingEnvironment)
+        {
+            _db = db;
+            //shit Idk why tf this line is underlined
+            _hostingEnvironment = hostingEnvironment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
