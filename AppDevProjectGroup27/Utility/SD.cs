532d36137fc7a0cd990a2f400d76fb9c2d1102e2
Rydell moutain Thomas <21900109@dut4life.ac.za>
2021-05-19 19:02:02 +0200

    The Create Post code was added to the MenuItem Controller Code for uploading Images to the Database And added a static Class for Constant Varaibles instead of using magic strings

---
+++ AppDevProjectGroup27/Utility/SD.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppDevProjectGroup27.Utility
+{
+    public static class SD
+    {
+        public const string DefaultFoodImage = "fast-food.png";
+    }
+}
