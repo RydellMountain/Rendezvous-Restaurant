f11e44d8f3a15d5d7274a11549fdfdf667f8c5b2
Rydell moutain Thomas <21900109@dut4life.ac.za>
2021-05-19 17:23:59 +0200

    Created the MenuItemViewModel Added Create Code to MenuItems Controller

--- AppDevProjectGroup27/Areas/Admin/Controllers/MenuItemsController.cs
+++ AppDevProjectGroup27/Areas/Admin/Controllers/MenuItemsController.cs
@@ -1,4 +1,5 @@
 ﻿using AppDevProjectGroup27.Data;
+using AppDevProjectGroup27.Models.ViewModels;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -15,10 +16,24 @@ namespace AppDevProjectGroup27.Areas.Admin.Controllers
         private readonly ApplicationDbContext _db;
 
         private readonly IWebHostEnvironment _hostingEnvironment;
+
+        [BindProperty]
+        public MenuItemViewModel MenuItemVM { get; set; }
+        //Allows us to not parse this as an agruement in the Create Code
+
+
+
+
         public MenuItemsController(ApplicationDbContext db, IWebHostEnvironment hostingEnvironment)
         {
             _db = db;
            _hostingEnvironment = hostingEnvironment;
+            MenuItemVM = new MenuItemViewModel()
+            {
+                Category = _db.Category,
+                MenuItems = new Models.MenuItems()
+
+            };
         }
 
         public async Task<IActionResult> Index()
@@ -27,6 +42,12 @@ namespace AppDevProjectGroup27.Areas.Admin.Controllers
             var menuItems = await _db.MenuItems.Include(m => m.Category).Include(m => m.SubCategory).ToListAsync();
 
             return View(menuItems);
+        }
+
+        //GET-Create
+        public IActionResult Create()
+        {
+            return View(MenuItemVM);
         }
     }
 }
