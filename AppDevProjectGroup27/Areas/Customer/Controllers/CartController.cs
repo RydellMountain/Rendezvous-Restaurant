f7cd4d459bca270ac9d1fd7f0ace615d29cad835
spheleletholy262000@gmail.com <spheleletholy262000@gmail.com@users.noreply.github.com>
2021-05-30 12:27:00 +0200

    Added Cart Controller & Index Action

---
+++ AppDevProjectGroup27/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,66 @@
+﻿using AppDevProjectGroup27.Data;
+using AppDevProjectGroup27.Models;
+using AppDevProjectGroup27.Models.ViewModels;
+using AppDevProjectGroup27.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AppDevProjectGroup27.Areas.Customer.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public OrderDetailsCart detailsCart { get; set; }
+
+        public CartController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+
+            detailsCart = new OrderDetailsCart()
+            {
+                OrderHeader = new Models.OrderHeader()
+            };
+
+            detailsCart.OrderHeader.OrderTotal = 0;
+
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            var cart = _db.ShoppingCart.Where(m => m.ApplicationUserId == claims.Value);
+
+            if(cart != null)
+            {
+                detailsCart.listCart = cart.ToList();
+            }
+
+            //To calculate the order total
+            foreach(var list in detailsCart.listCart)
+            {
+                list.MenuItems = await _db.MenuItems.FirstOrDefaultAsync(m => m.Id == list.MenuItemId);
+                detailsCart.OrderHeader.OrderTotal = detailsCart.OrderHeader.OrderTotal + (list.MenuItems.Price + list.Count);
+                list.MenuItems.Descriptions = SD.ConvertToRawHtml(list.MenuItems.Descriptions);
+
+                if (list.MenuItems.Descriptions.Length>100)
+                {
+                    list.MenuItems.Descriptions = list.MenuItems.Descriptions.Substring(0, 99) + "...";
+
+                }
+
+            }
+            detailsCart.OrderHeader.OrderTotal = detailsCart.OrderHeader.OrderTotal;
+
+            return View(detailsCart);
+
+        }
+    }
+}
