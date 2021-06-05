1824d756d5d2ded3974160db6212641514253896
Damian Govender <21912602@dut4life.ac.za>
2021-06-05 12:26:32 +0200

    Added Cancel method in OrderController and Cancel View in Order View.

--- AppDevProjectGroup27/Areas/Customer/Controllers/OrderController.cs
+++ AppDevProjectGroup27/Areas/Customer/Controllers/OrderController.cs
@@ -1,5 +1,6 @@
 ﻿using AppDevProjectGroup27.Data;
 using AppDevProjectGroup27.Models.ViewModels;
+using AppDevProjectGroup27.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -35,6 +36,34 @@ namespace AppDevProjectGroup27.Areas.Customer.Controllers
 
             };
 
+            orderDetailsViewModel.OrderHeader.PaymentStatus = SD.PaymentStatusApproved;
+            orderDetailsViewModel.OrderHeader.Status = SD.StatusSubmitted;
+
+            await _db.SaveChangesAsync();
+
+            return View(orderDetailsViewModel);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Cancel(int id)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            OrderDetailsViewModel orderDetailsViewModel = new OrderDetailsViewModel()
+            {
+                OrderHeader = await _db.OrderHeader.Include(o => o.ApplicationUser).FirstOrDefaultAsync(o => o.Id == id && o.UserId == claim.Value),
+                OrderDetails = await _db.OrderDetails.Where(o => o.OrderId == id).ToListAsync()
+
+            };
+
+
+            //Changes
+            orderDetailsViewModel.OrderHeader.PaymentStatus = SD.PaymentStatusRejected;
+            orderDetailsViewModel.OrderHeader.Status = SD.StatusCancelled;
+
+            await _db.SaveChangesAsync();
+            //Changes
             return View(orderDetailsViewModel);
         }
     }
